5c871315cd9f51aee5edde4f1da173c4a3bd5151
pantho0 <panthomashreky6@gmail.com>
2023-10-18 08:32:56 +0600

    initialized project & added navar

---
+++ src/Pages/Home/Home.jsx
@@ -0,0 +1,13 @@
+import { Outlet } from "react-router-dom";
+import Navbar from "./Navbar/Navbar";
+
+
+const Home = () => {
+    return (
+        <div>
+            Home
+        </div>
+    );
+};
+
+export default Home;
---
+++ src/Router/Router.jsx
@@ -0,0 +1,30 @@
+import {
+    createBrowserRouter,
+  } from "react-router-dom";
+
+import Home from "../Pages/Home/Home";
+import MainLayout from "../MainLayout/MainLayout";
+import Signup from "../Pages/Signup/Signup";
+import Login from "../Pages/Login/Login";
+
+export const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <MainLayout></MainLayout>,
+      children:[
+        {
+          path:"/",
+          element : <Home></Home>
+        },
+        {
+          path: '/signup',
+          element: <Signup></Signup>
+        },
+        {
+          path: '/login',
+          element: <Login></Login>
+        }
+      ]
+    },
+  ]);
+
