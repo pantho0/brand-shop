c203831b3f6e2941b8991ce40df0fcb5a933c582
pantho0 <panthomashreky6@gmail.com>
2023-10-19 14:04:40 +0600

    slider with imges added to homepage

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,15 +1,48 @@
-import { Outlet } from "react-router-dom";
-import Navbar from "./Navbar/Navbar";
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { Navigation} from 'swiper/modules';
 
-
+import 'swiper/swiper-bundle.css';
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+import 'swiper/css/scrollbar';
 
 const Home = () => {
-   
-    return (
-        <div>
-            
-        </div>
-    );
+  return (
+    <div>
+      {/* Slider Section */}
+      <div className='h-screen'>
+      <Swiper
+       modules={[Navigation]}
+       spaceBetween={0}
+       slidesPerView={2}
+       navigation
+       pagination={{ clickable: true }}
+       scrollbar={{ draggable: true }}
+       onSwiper={(swiper) => console.log(swiper)}
+       onSlideChange={() => console.log('slide change')}
+    >
+      <SwiperSlide>
+        <img className='h-[768px] w-full' src="/1.jpg" alt="" />
+      </SwiperSlide>
+      <SwiperSlide>
+        <img className='h-[768px] w-full' src="/2.jpg" alt="" />
+      </SwiperSlide>
+      <SwiperSlide>
+        <img className='h-[768px] w-full' src="/3.jpg" alt="" />
+      </SwiperSlide>
+      <SwiperSlide>
+        <img className='h-[768px] w-full' src="/4.jpg" alt="" />
+      </SwiperSlide>
+      <SwiperSlide>
+        <img className='h-[768px] w-full' src="/5.jpg" alt="" />
+      </SwiperSlide>
+
+
+    </Swiper>
+      </div>
+    </div>
+  );
 };
 
 export default Home;
